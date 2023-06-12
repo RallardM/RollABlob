736ff7fcf22a7595979c653f6c250c606ef74ec8
Remi Allard Mayer <remallmay@outlook.com>
2023-06-12 12:25:10 -0400

    corrected some props

--- Assets/Scripts/BlobAbsorb.cs
+++ Assets/Scripts/BlobAbsorb.cs
@@ -1,62 +1,110 @@
-// Source : https://docs.unity3d.com/ScriptReference/Transform.SetParent.html
-// Source : https://forum.unity.com/threads/rotation-is-not-being-inherited-from-parent.822780/
 // Source : https://answers.unity.com/questions/37286/how-turn-off-the-mesh-collider.html
+// Source: https://forum.unity.com/threads/cuberoot.33513/
+// Source : https://docs.unity3d.com/ScriptReference/Transform-localScale.html
+// Source : https://forum.unity.com/threads/getting-the-position-of-a-parent-gameobject.1138150/
+// Source : https://gamedevbeginner.com/the-right-way-to-lerp-in-unity-with-examples/
 
-using Unity.VisualScripting;
-using UnityEditor.PackageManager.Requests;
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class BlobAbsorb : MonoBehaviour
 {
-    private Transform m_playerNode;
+    private Transform m_playerTransform;
+    private MeshCollider m_playerMeshCollider;
+    private Vector3 m_playerInitialSize = Vector3.zero;
+    private float m_playerInitialVolume = 0.0f;
+    private float m_assetMassToAdd = 0.0f;
+    //private float m_playerInitialMass = 0.0f;
+    private float m_lerpSpeed = 8f; // Divide by 2 or multiply by 0.5, higher divider or smaller multiplier, faster lerp
 
     private void Awake()
     {
-        m_playerNode = transform.parent.GetComponent<Transform>();
+        float colliderHeight = 0.0f;
+        float colliderWidth = 0.0f;
+        float colliderDepth = 0.0f;
+
+        // Source : https://forum.unity.com/threads/getting-the-position-of-a-parent-gameobject.1138150/
+        m_playerTransform = transform.parent.transform;
+        m_playerMeshCollider = m_playerTransform.GetComponent<MeshCollider>();
+
+        // Source : https://docs.unity3d.com/ScriptReference/Collider-bounds.html
+        colliderHeight = m_playerMeshCollider.bounds.size.y;
+        colliderWidth = m_playerMeshCollider.bounds.size.x;
+        colliderDepth = m_playerMeshCollider.bounds.size.z;
+        m_playerInitialVolume = colliderWidth * colliderHeight * colliderDepth;
+        m_playerInitialSize = GetPlayerNewSize();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log("Object entered : " + other.name);
+        
         if (other.gameObject.tag == "NPC")
         {
+            Debug.Log("Object entered : " + other.name);
             other.gameObject.GetComponent<NavMeshAgent>().enabled = false;
             other.gameObject.GetComponent<IBrain>().enabled = false;
             other.gameObject.GetComponent<EatenAsset>().IsBeingEaten = true;
-            other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            //other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            m_assetMassToAdd += other.gameObject.GetComponent<Rigidbody>().mass;
+            other.gameObject.tag = "Eaten";
         }
-            
+
         if (other.gameObject.tag == "Movable")
         {
-            //Vector3 objectScale = other.transform.localScale;
-            //RemoveCollider(other.gameObject);
-            //DetachFromParent(other.gameObject);
-            //SetPlayerNodeParent(other.gameObject);
-            //other.transform.localScale = objectScale;
+
         }
     }
 
-    // Source : https://answers.unity.com/questions/37286/how-turn-off-the-mesh-collider.html
-    private void RemoveCollider(GameObject child)
+    void FixedUpdate()
     {
-        //Removes the collider component from the GameObject "child".
-        Destroy(child.GetComponent<Collider>());
+        //Debug.Log("Object mass to add : " + m_assetMassToAdd);
+        if (m_assetMassToAdd > 0.0f)
+        {
+            Vector3 previousPlayerSize = Vector3.zero;
+            Vector3 newPlayerSize = Vector3.zero;
+            Vector3 playerSizeVolumeDifference = Vector3.zero;
+            float newVolumeToAddToPLayer = 0.0f;
+            float massAddedToSubstract = 0.0f;
+
+            // Source: https://forum.unity.com/threads/cuberoot.33513/
+            // Source : https://docs.unity3d.com/ScriptReference/Transform-localScale.html
+            // Get the cube root of the added mass to get one of the lenght of the mass as a cube and add this length to the player's cubic lenghts
+            newVolumeToAddToPLayer = Mathf.Pow(m_assetMassToAdd, 1f / 3f);
+            previousPlayerSize = GetPlayerNewSize();
+
+            // Source : https://gamedevbeginner.com/the-right-way-to-lerp-in-unity-with-examples/
+            float xLerp = Mathf.Lerp(previousPlayerSize.x, previousPlayerSize.x + newVolumeToAddToPLayer, Time.fixedDeltaTime / m_lerpSpeed);
+            float yLerp = Mathf.Lerp(previousPlayerSize.y, previousPlayerSize.y + newVolumeToAddToPLayer, Time.fixedDeltaTime / m_lerpSpeed);
+            float zLerp = Mathf.Lerp(previousPlayerSize.z, previousPlayerSize.z + newVolumeToAddToPLayer, Time.fixedDeltaTime / m_lerpSpeed);
+
+            // Resize the player scale to the lerping new scale
+            m_playerTransform.localScale = new Vector3(xLerp, yLerp, zLerp);
+
+            // Update the player's mesh collider to the new scale
+            m_playerMeshCollider.GetComponent<MeshCollider>().sharedMesh = null;
+            m_playerMeshCollider.GetComponent<MeshCollider>().sharedMesh = m_playerTransform.GetComponent<MeshFilter>().mesh;
+
+            // Get the difference between the new player size and the initial player size
+            newPlayerSize = GetPlayerNewSize();
+            playerSizeVolumeDifference = newPlayerSize - m_playerInitialSize;
+            massAddedToSubstract = Mathf.Pow(playerSizeVolumeDifference.x, 3f);
+
+            //Debug.Log("Object mass to add before substraction : " + m_assetMassToAdd);
+            // Substract that difference to the mass to add
+            m_assetMassToAdd -= massAddedToSubstract;
+            //Debug.Log("Object mass to add after substraction : " + m_assetMassToAdd);
+        }
     }
 
-    // Source : https://docs.unity3d.com/ScriptReference/Transform.SetParent.html
-    private void DetachFromParent(GameObject child)
+    private Vector3 GetPlayerNewSize()
     {
-        // Detaches the transform from its parent.
-        child.transform.SetParent(null);
-    }
-    private void SetPlayerNodeParent(GameObject child)
-    {
-        //Vector3 objectScale = child.transform.localScale;
-        //Debug.Log("Object scale before : " + objectScale);
-        //Makes the GameObject "player" the parent of the GameObject "child".
-        child.transform.SetParent(m_playerNode);
-        //child.transform.localScale = objectScale;
-        //Debug.Log("Object scale after : " + child.transform.localScale);
+        float colliderHeight = 0.0f;
+        float colliderWidth = 0.0f;
+        float colliderDepth = 0.0f;
+        colliderHeight = m_playerMeshCollider.bounds.size.y;
+        colliderWidth = m_playerMeshCollider.bounds.size.x;
+        colliderDepth = m_playerMeshCollider.bounds.size.z;
+        return new Vector3(colliderWidth, colliderHeight, colliderDepth);
     }
 }
--- Assets/Scripts/EatenAsset.cs
+++ Assets/Scripts/EatenAsset.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EatenAsset : MonoBehaviour
 {
     GameObject m_playerNode;
+    private float m_lerpSpeed = 2f; // Divide by 2 or multiply by 0.5, higher divider or smaller multiplier, faster lerp
+    private float m_safeDistanceBeforeBeingEaten = 1f;
     private bool m_isBeingEaten = false;
     private bool m_isEaten = false;
 
@@ -13,6 +15,47 @@ public class EatenAsset : MonoBehaviour
     private void Awake()
     {
         m_playerNode = GameObject.FindGameObjectWithTag("PlayerBlob");
+        float radius = 0.0f;
+        float radiusSurface = 0.0f;
+        float sphereVolume = 0.0f;
+        float colliderHeight = 0.0f;
+        float colliderWidth = 0.0f;
+        float colliderDepth = 0.0f;
+        float colliderVolume = 0.0f;
+        
+        // Checks what is the current object collider to assign it's mass based on its collider volume
+        if (GetComponent<CapsuleCollider>())
+        {
+            radius = GetComponent<CapsuleCollider>().radius;
+            colliderHeight = GetComponent<CapsuleCollider>().height;
+            radiusSurface = (radius * radius) * Mathf.PI;
+            sphereVolume = ((radius * radius * radius) * Mathf.PI) * (4/3);
+            colliderVolume = (radiusSurface * colliderHeight) + sphereVolume;
+            GetComponent<Rigidbody>().mass = colliderVolume;
+        }
+        else if (GetComponent<SphereCollider>())
+        {
+            radius = GetComponent<SphereCollider>().radius;
+            sphereVolume = ((radius * radius * radius) * Mathf.PI) * (4 / 3);
+            GetComponent<Rigidbody>().mass = sphereVolume;
+        }
+        else if (GetComponent<BoxCollider>())
+        {
+            colliderHeight = GetComponent<BoxCollider>().size.y;
+            colliderWidth = GetComponent<BoxCollider>().size.x;
+            colliderDepth = GetComponent<BoxCollider>().size.z;
+            colliderVolume = colliderHeight * colliderWidth * colliderDepth;
+            GetComponent<Rigidbody>().mass = colliderVolume;
+        }
+        // Source : https://docs.unity3d.com/ScriptReference/Collider-bounds.html
+        else if (GetComponent<MeshCollider>())
+        {
+            colliderHeight = GetComponent<MeshCollider>().bounds.size.y;
+            colliderWidth = GetComponent<MeshCollider>().bounds.size.x;
+            colliderDepth = GetComponent<MeshCollider>().bounds.size.z;
+            colliderVolume = colliderWidth * colliderHeight * colliderDepth;
+            GetComponent<Rigidbody>().mass = colliderVolume;
+        }
     }
 
     void FixedUpdate()
@@ -20,17 +63,17 @@ public class EatenAsset : MonoBehaviour
         if (m_isBeingEaten)
         {
             //transform.position = Vector3.Lerp(transform.position, m_playerNode.transform.position, Time.fixedDeltaTime);
-            if (Vector3.Distance(transform.position, m_playerNode.transform.position) < 0.4f)
+            if (Vector3.Distance(transform.position, m_playerNode.transform.position) < m_safeDistanceBeforeBeingEaten)
             {
                 m_isBeingEaten = false;
                 m_isEaten = true;
             }
             else 
             {
-                transform.position = Vector3.Lerp(transform.position, m_playerNode.transform.position, Time.fixedDeltaTime * 2f);
+                transform.position = Vector3.Lerp(transform.position, m_playerNode.transform.position, Time.fixedDeltaTime / m_lerpSpeed);
 
                 // Source : https://answers.unity.com/questions/1574465/error-cannot-convert-quaternion-to-vector3.html
-                transform.rotation = Quaternion.Lerp(transform.rotation, m_playerNode.transform.rotation, Time.fixedDeltaTime * 2f);
+                transform.rotation = Quaternion.Lerp(transform.rotation, m_playerNode.transform.rotation, Time.fixedDeltaTime / m_lerpSpeed);
             }
         }
 
